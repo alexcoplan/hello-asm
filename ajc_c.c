70399d2f08e99c6bde507931b5ae625cbacbfa82
Alex Coplan <alex@coplan.io>
2020-01-15 16:09:15 +0000

    add atoi impl

---
+++ ajc_c.c
@@ -0,0 +1,29 @@
+bool ajc_atoi_c(const char *str, int *result)
+{
+  const unsigned neg = (*str == '-');
+  if (neg)
+    str++;
+
+  // "" and "-" are invalid.
+  if (!str[0])
+    return false;
+
+  // Leading zero is only valid if it's the only digit.
+  if (str[0] == '0' && str[1] != '\0')
+    return false;
+
+  int acc = 0;
+  for (; *str; str++) {
+    if (*str < '0' || *str > '9')
+      return false;
+
+    if (acc > INT_MAX / 10)
+      return false; // Overflow
+
+    acc *= 10;
+    acc += (str - '0');
+  }
+
+  *result = (neg * -1) * acc;
+  return true;
+}
---
+++ ajc_c.h
@@ -0,0 +1,3 @@
+// On success, returns true and sets result.
+// On failure (overflow, invalid input), returns false.
+bool ajc_atoi_c(const char *str, int *result);
